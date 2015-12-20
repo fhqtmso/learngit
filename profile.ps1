d4ea5b27ae8b94884c1d17bdbab518db15938e16
fhqtmso <fhqtmso@gmail.com>
2015-12-20 20:50:01 +0800

    Powershell variables for Git Repositories pathes

--- profile.ps1
+++ profile.ps1
@@ -10,3 +10,7 @@
 $env:Path += ";E:\Onedrive\WEIPAN\Sublime Text Build 3083 x64"
 Remove-Item Alias:cd
 function cd { if ($args[0] -eq '-') { $pwd=$OLDPWD; } else { $pwd=$args[0]; } $tmp=pwd; if ($pwd) { Set-Location $pwd; } Set-Variable -Name OLDPWD -Value $tmp -Scope global; }
+# PATH Variables for Git repositories
+$layout = "E:\Onedrive\WEIPAN\Ledit-Layouts\"
+$snippet = "E:\Onedrive\WEIPAN\Sublime Text Build 3083 x64\Data\Packages\User\ledit-snippets"
+$LeditInclude = "D:\Program Files (x86)\Tanner EDA\L-Edit 11.1\upi\Interpreted_Include/"
